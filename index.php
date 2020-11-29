06c4db8b1f8fdff27b84c48f9f49a34699144f87
abbasShDev <abbas20alzaeem@gmail.com>
2020-11-29 20:10:58 +0300

    design home page done

---
+++ index.php
@@ -0,0 +1,235 @@
+
+<!doctype html>
+<html lang="ar" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="AbbasShDev @AbbasShDev">
+<!--    <link rel="icon" type="image/png" href="layout/images/favicon.svg">-->
+    <!-- Fonts -->
+    <link rel="stylesheet" href="layout/css/fonts/neo.ttf">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css" integrity="sha384-vus3nQHTD+5mpDiZ4rkEPlnkcyTP+49BhJ4wJeJunw06ZAp+wzzeBPUXr42fi8If" crossorigin="anonymous">
+    <!-- main CSS -->
+    <link rel="stylesheet" href="layout/css/main.css">
+    <title></title>
+</head>
+<body>
+
+<!-- Start navbar  -->
+<nav class="navbar navbar-expand-sm bg-dark navbar-light">
+    <div class="container">
+        <a href="#" class="navbar-brand">تدويناتي</a>
+        <button class="navbar-toggler border-0" data-toggle="collapse" data-target="#menu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="menu">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">عن المدونة</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">شروحات</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">منوعات</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">تواصل معنا</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- End navbar  -->
+
+<!-- Start Content  -->
+<div class="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="post">
+                    <div class="post-image">
+                        <img src="layout/images/default-post.png" alt="post image">
+                    </div>
+                    <div class="post-title">
+                        <h4>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</h4>
+                    </div>
+                    <div class="post-details">
+                        <p class="post-info">
+                            <span><i class="fas fa-user"></i>عبدالله محمد</span>
+                            <span><i class="far fa-calendar-alt"></i>29/11/2020</span>
+                            <span><i class="fas fa-tags"></i>بلوجر</span>
+                        </p>
+                        <p>لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
+                            إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد
+                        </p>
+                        <button class="btn btn-custom">إقرأ المزيد</button>
+                    </div>
+                </div>
+                <div class="post">
+                    <div class="post-image">
+                        <img src="layout/images/default-post2.png" alt="post image">
+                    </div>
+                    <div class="post-title">
+                        <h4>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</h4>
+                    </div>
+                    <div class="post-details">
+                        <p class="post-info">
+                            <span><i class="fas fa-user"></i>عبدالله محمد</span>
+                            <span><i class="far fa-calendar-alt"></i>29/11/2020</span>
+                            <span><i class="fas fa-tags"></i>بلوجر</span>
+                        </p>
+                        <p>لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
+                            إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد
+                        </p>
+                        <button class="btn btn-custom">إقرأ المزيد</button>
+                    </div>
+                </div>
+                <div class="post">
+                    <div class="post-image">
+                        <img src="layout/images/default-post3.png" alt="post image">
+                    </div>
+                    <div class="post-title">
+                        <h4>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</h4>
+                    </div>
+                    <div class="post-details">
+                        <p class="post-info">
+                            <span><i class="fas fa-user"></i>عبدالله محمد</span>
+                            <span><i class="far fa-calendar-alt"></i>29/11/2020</span>
+                            <span><i class="fas fa-tags"></i>بلوجر</span>
+                        </p>
+                        <p>لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
+                            إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد
+                        </p>
+                        <button class="btn btn-custom">إقرأ المزيد</button>
+                    </div>
+                </div>
+                <div class="post">
+                    <div class="post-image">
+                        <img src="layout/images/default-post2.png" alt="post image">
+                    </div>
+                    <div class="post-title">
+                        <h4>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</h4>
+                    </div>
+                    <div class="post-details">
+                        <p class="post-info">
+                            <span><i class="fas fa-user"></i>عبدالله محمد</span>
+                            <span><i class="far fa-calendar-alt"></i>29/11/2020</span>
+                            <span><i class="fas fa-tags"></i>بلوجر</span>
+                        </p>
+                        <p>لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.
+                            إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد
+                        </p>
+                        <button class="btn btn-custom">إقرأ المزيد</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <!-- Start categories -->
+                <div class="categories">
+                    <h4 class="mx-auto">
+                        التصنيفات
+                        <span></span>
+                    </h4>
+                    <ul>
+                        <a href="">
+                            <li>
+                                <span><i class="fas fa-tags"></i></span>
+                                <span>بلوجر</span>
+                            </li>
+                        </a>
+                        <a href="">
+                            <li>
+                                <span><i class="fas fa-tags"></i></span>
+                                <span>يوتيوب</span>
+                            </li>
+                        </a>
+                        <a href="">
+                            <li>
+                                <span><i class="fas fa-tags"></i></span>
+                                <span>دورات</span>
+                            </li>
+                        </a>
+                        <a href="">
+                            <li>
+                                <span><i class="fas fa-tags"></i></span>
+                                <span>ويب</span>
+                            </li>
+                        </a>
+                        <a href="">
+                            <li>
+                                <span><i class="fas fa-tags"></i></span>
+                                <span>اندرويد</span>
+                            </li>
+                        </a>
+                        <a href="">
+                            <li>
+                                <span><i class="fas fa-tags"></i></span>
+                                <span>ios</span>
+                            </li>
+                        </a>
+
+                    </ul>
+                </div>
+                <!-- End categories -->
+                <!-- Start latest-posts-->
+                <div class="latest-posts">
+                    <h4 class="mx-auto">
+                        أحدث المنشورات
+                        <span></span>
+                    </h4>
+                    <ul>
+                        <li>
+                            <a href="">
+                                <span class="span-image"><img src="layout/images/default-post.png" alt="latest posts image"></span>
+                                <span>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <span class="span-image"><img src="layout/images/default-post3.png" alt="latest posts image"></span>
+                                <span>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <span class="span-image"><img src="layout/images/default-post2.png" alt="latest posts image"></span>
+                                <span>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- End latest-posts -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Content  -->
+
+<!-- Start Footer  -->
+<!--<div class="footer">-->
+<!--    <div>-->
+<!--        <a href="https://twitter.com/AbbasShDev" target="_blank"><i class="mr-2 p-1 fab fa-twitter"></i></a>-->
+<!--        <a href="https://github.com/AbbasShDev" target="_blank"><i class="mr-2 p-1 fab fa-github "></i></a>-->
+<!--        <a href="https://www.linkedin.com/in/abbas-alshaqaq-7b58221a5/" target="_blank"><i class="mr-2 p-1 fab fa-linkedin"></i></a>-->
+<!--        Coded by AbbasShDev <span>2020 ©</span>-->
+<!--    </div>-->
+<!--</div>-->
+<!-- End Footer  -->
+<!-- jQuery js -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<!-- Popper.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<!-- Bootstrap js -->
+<script src="https://cdn.rtlcss.com/bootstrap/v4.2.1/js/bootstrap.min.js" integrity="sha384-a9xOd0rz8w0J8zqj1qJic7GPFfyMfoiuDjC9rqXlVOcGO/dmRqzMn34gZYDTel8k" crossorigin="anonymous"></script>
+<!-- main js -->
+<script src="layout/js/main.js"></script>
+</body>
+</html>
